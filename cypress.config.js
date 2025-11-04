fbfd512d8035095378a47f9768c5a249d084bce2
krithika <krithika.r@datman.je>
2025-11-04 20:20:39 +0530

    updated cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,15 @@
-
-
-
 require("dotenv").config();
 
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.WING_URL,
+    baseUrl: process.env.WING_URL || "https://test.wing.work",
     env: {
       WING_USERNAME: process.env.WING_USERNAME,
       WING_PASSWORD: process.env.WING_PASSWORD,
       screenshotOnRunFailure: true,
       video: false,
-
     },
   },
 });
-
